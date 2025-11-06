dc04081a6d05fb484f44078e74dbca9d2cfb2122
pkpoornima27 <pk.poornima@gmail.com>
2025-11-06 10:52:58 +0000

    Add_two_numbers

---
+++ src/2_Add_two_numbers.py
@@ -0,0 +1,10 @@
+""" 2. Add two numbers."""
+
+first_num = int(input("Enter the first number:"))
+second_num = int(input("Enter the second number:"))
+
+def addition(num1, num2):
+    sum_num = num1 + num2
+    return sum_num
+
+print(addition(first_num, second_num))
