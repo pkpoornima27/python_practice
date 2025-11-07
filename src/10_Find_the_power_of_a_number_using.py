648812a6eefe887476c7528ab42ddc8579bde349
pkpoornima27 <pk.poornima@gmail.com>
2025-11-07 14:43:25 +0000

    10_Find_the_power_of_a_number_using

---
+++ src/10_Find_the_power_of_a_number_using.py
@@ -0,0 +1,7 @@
+"""10. Find the power of a number using **"""
+
+number = int(input("Enter the number"))
+power_number = int(input("Enter the power number"))
+
+final_value = number ** power_number
+print(f"{number} power {power_number} is equal to {final_value}")
