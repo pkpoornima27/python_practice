8d593605e4a1df84a7298623f9c4397dc9bdeee7
pkpoornima27 <pk.poornima@gmail.com>
2025-11-08 21:32:15 +0000

    16_Reverse_a_number

--- src/16_Reverse_a_number.py
+++ src/16_Reverse_a_number.py
@@ -1,9 +1,9 @@
 """ 16. Reverse a number"""
 
 num = 987654321
-reverse = " "
+reverse = 0
 while num != 0:
      rem = num % 10
-     reverse = reverse + str(rem)
+     reverse = reverse * 10 + rem
      num = num // 10
-print(int(reverse))
+print(reverse)
