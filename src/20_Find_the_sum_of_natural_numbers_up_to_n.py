20ed51a048f59c51b0c74f796c9a964b7afdb11c
pkpoornima27 <pk.poornima@gmail.com>
2025-11-10 14:32:32 +0000

    20_Find_the_sum_of_natural_numbers_up_to_n

---
+++ src/20_Find_the_sum_of_natural_numbers_up_to_n.py
@@ -0,0 +1,5 @@
+"""20. Find the sum of natural numbers up to n"""
+
+num = int(input("Enter the num"))
+sum_of_all_num = (num * (num +1))//2
+print(f"Sum of all number till {num}: {sum_of_all_num}")
