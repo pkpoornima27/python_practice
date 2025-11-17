0f80d45fbeafb7066614dd7041b385d96ff4d345
pkpoornima27 <pk.poornima@gmail.com>
2025-11-17 14:43:49 +0000

    44_Reverse_a_list

---
+++ src/44_Reverse_a_list.py
@@ -0,0 +1,21 @@
+""" 44. Reverse a list"""
+
+l1 = [4, 6, 2, 7, 1, 8, 0, 4]
+print(l1)
+
+def reverse_list_manually(l_list):
+    reverse = []
+    size = len(l_list)
+    for i in range(size-1, -1, -1 ):
+        reverse.append(l_list[i])
+    return reverse
+
+print("Reversed list:", reverse_list_manually(l1))
+l1.reverse()
+print("Reversed list using built in function:", l1)
+
+
+
+
+
+
